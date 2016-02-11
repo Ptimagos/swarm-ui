8c5b5ec46106336167f7a98c0770955f41f2a925
Ptimagos <david.lepoultier@gmail.com>
2016-02-11 13:10:30 +0100

    Update javascript.php

--- swarm-ui/www/common/javascript.php
+++ swarm-ui/www/common/javascript.php
@@ -3,26 +3,6 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script type='text/javascript' src='js/bootstrap.min.js'></script>
 
-<!-- Loader animation 
-<script type='text/javascript' src='js/canvas.js'></script>
--->
-
-<!-- Highcharts 
-<script type="text/javascript" src="js/highstock.js"></script>
-<script type="text/javascript" src="js/modules/exporting.js"></script>
-<script type="text/javascript" src="js/highslide-full.min.js"></script>
-<script type="text/javascript" src="js/highslide.config.js" charset="utf-8"></script>
-<script type="text/javascript" src="js/highcharts-more.js"></script>
--->
-
-<!-- Login ajax 
-<script type="text/javascript" src="js/login.js"></script>
--->
-
-<!-- loading modal 
-<script type="text/javascript" src="js/loading.js"></script>
--->
-
 <!-- Hidden the loader
 <script type='text/javascript'>
 	function load(){
